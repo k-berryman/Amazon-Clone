b90700d92d889075ca430ddb2bdb8e5549801b16
Kaitlin Berryman <kaitlinberryman@gmail.com>
2022-03-04 18:57:38 -0500

    Add data layer

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,28 @@
 import React from 'react'
 import './Product.css'
+import { useStateValue } from "./StateProvider";
 
 function Product({ id, title, image, price, rating }) {
+  // Destructure basket out of state
+  const [{ basket }, dispatch] = useStateValue();
+
+  console.log('basketttt', basket);
+
+  const addToBasket = () => {
+    // Dispatch the item into the data layer
+    // Shoot it into the data layer!!! Zoom
+    dispatch({
+      type: 'ADD_TO_BASKET',
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
+
   return (
     <div className="product">
       <div className="product__info">
@@ -25,7 +46,8 @@ function Product({ id, title, image, price, rating }) {
       />
 
 
-      <button>Add to Basket</button>
+      <button
+        onClick={addToBasket}>Add to Basket</button>
 
     </div>
   )
